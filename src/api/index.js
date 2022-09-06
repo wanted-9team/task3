873364352ae57b46459266aeaed00ef9b6a803d0
jungdeokwoo <dogsocks@dogsocksui-MacBookPro.local>
2022-09-07 00:05:42 +0900

    [ADD]axios관련 세팅
    axios instance생성, 호출api생성

--- src/api/index.js
+++ src/api/index.js
@@ -1,1 +1,14 @@
-//api 관련
+import axios from 'axios'
+
+const BASE_URL = process.env.REACT_APP_API_URL
+
+export const Axios = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    'Content-type': 'application/json',
+  },
+  params: {
+    api_key: process.env.REACT_APP_SERVICE_KEY,
+    language: 'ko',
+  },
+})
