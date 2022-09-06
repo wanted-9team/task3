85a2fed208a31386ada76dd614a1c69c4bed3675
OH YOUNGJAE <85508157+sacultang@users.noreply.github.com>
2022-09-07 03:48:23 +0900

    [ADD] MoveList 페이지 무한스크롤 구현 (#4)
    * [ADD] react-intersection-observer 설치
    인피니트 스크롤 구현 위해 react-intersection-observer 패키지 설치
    
    * [ADD] Header NavLink 작성
    라우팅 확인 위해 헤더에 NavLink 추가
    
    * [ADD] MoveList 페이지 무한스크롤 구현
    react-query의 useInfiniteQuery 이용해 무한 스크롤 구현

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
-
+import { NavLink } from 'react-router-dom'
 const Header = () => {
-  return <div>Header</div>
+  return (
+    <div>
+      <NavLink to="/upcoming">upcoming</NavLink>
+      <NavLink to="/now_playing">nowplaying</NavLink>
+      <NavLink to="/top_rated">upcoming</NavLink>
+    </div>
+  )
 }
 
 export default Header
--- src/utils/MovieApi.js
+++ src/utils/MovieApi.js
@@ -3,3 +3,19 @@ import { Axios } from 'api'
 export const popularMovieApi = async () => {
   return await Axios.get('/movie/popular')
 }
+export const getMovieList = async (url, page) => {
+  try {
+    const res = await Axios({
+      url: `/movie${url}`,
+      method: 'get',
+      params: {
+        page,
+      },
+    })
+
+    return res.data
+  } catch (err) {
+    console.log(err)
+    return err
+  }
+}
