4d2640d56d00f6b23a5a27d02a83d08d4d5d7837
Jaehyeon <jaehyeonp74@gmail.com>
2022-09-07 18:58:34 +0900

    [ADD] 헤더 바 레이아웃 구현 및 카드 css 수정
    - 헤더 반응형 구현
    - 카드 default 이미지
    - 카드 컴포넌트 width, height 디폴트로 정하기
    - 카드 별점 위치 수정

--- src/pages/MovieMain/MovieMain.jsx
+++ src/pages/MovieMain/MovieMain.jsx
@@ -2,11 +2,7 @@ import React from 'react'
 import MovieCard from 'components/MovieCard'
 
 const MovieMain = () => {
-  return (
-    <div>
-      <MovieCard poster={'/AfvIjhDu9p64jKcmohS4hsPG95Q.jpg'} vote={7.9} title={'블랙폰'} />
-    </div>
-  )
+  return <></>
 }
 
 export default MovieMain
